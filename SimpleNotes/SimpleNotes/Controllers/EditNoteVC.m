4a580ce567e8d4a02dd8bff2f2dc6325abf84185
Oleg Sannikov <lgn.sanni@gmail.com>
2016-03-28 23:17:48 +0600

    EditNoteVC initial implementation

--- SimpleNotes/SimpleNotes/Controllers/EditNoteVC.m
+++ SimpleNotes/SimpleNotes/Controllers/EditNoteVC.m
@@ -1,34 +1,73 @@
 
 #import "EditNoteVC.h"
+#import "DataManager.h"
+#import "Note+CoreDataProperties.h"
 
+#import <AssetsLibrary/AssetsLibrary.h>
 
-@interface EditNoteVC ()
+@interface EditNoteVC () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *addImageButton;
+
+@property (nonatomic) Note *tmpNote;
+@property (nonatomic) UIImagePickerController *imagePicker;
 
 @end
 
 
 @implementation EditNoteVC
 
+#pragma mark - Lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Do any additional setup after loading the view from its nib.
+    [self.addImageButton addTarget:self action:@selector(addImage:) forControlEvents:UIControlEventTouchUpInside];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+#pragma mark - Accessors
+
+- (void)setNote:(Note *)note {
+    [super setNote:note];
+    _tmpNote = [[DataManager sharedManager] newTemporaryNoteWithNote:note];
 }
 
-/*
+- (UIImagePickerController *)imagePicker {
+    if (!_imagePicker) {
+        _imagePicker = [UIImagePickerController new];
+        _imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        _imagePicker.allowsEditing = NO;
+        _imagePicker.delegate = self;
+    }
+    
+    return _imagePicker;
+}
+
+
+#pragma mark - Actions
+
+- (void)addImage:(id)sender {
+    [self presentViewController:self.imagePicker animated:YES completion:nil];
+}
+
+
+#pragma mark - UIImagePickerControllerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    NSURL *refURL = (NSURL *)info[UIImagePickerControllerReferenceURL];
+    Image *img = [[DataManager sharedManager] newImageWithReferenceURL:refURL];
+    tmp.note 
+    
+}
+
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    
 }
-*/
+
 
 @end
