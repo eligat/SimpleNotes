ac47d281436d24085d84759faeb508efd8107655
Oleg Sannikov <lgn.sanni@gmail.com>
2016-03-29 23:48:33 +0600

    changes in model

--- SimpleNotes/SimpleNotes/Model/Image.m
+++ SimpleNotes/SimpleNotes/Model/Image.m
@@ -6,4 +6,12 @@
 
 // Insert code here to add functionality to your managed object subclass
 
+- (void)prepareForDeletion{
+    NSLog(@"%s", __func__);
+}
+
+- (BOOL)validateForDelete:(NSError * _Nullable __autoreleasing *)error {
+    NSLog(@"%s", __func__);
+    return YES;
+}
 @end
--- SimpleNotes/SimpleNotes/Model/Note.m
+++ SimpleNotes/SimpleNotes/Model/Note.m
@@ -5,7 +5,20 @@
 
 - (void)awakeFromInsert{
     self.created = [NSDate date];
-    self.updated = self.created;
+    self.updated = [NSDate date];
+}
+
+// Bug http://stackoverflow.com/q/7385439/4398053
+- (void)addImagesObject:(Image *)value {
+    NSMutableOrderedSet* tempSet = [NSMutableOrderedSet orderedSetWithOrderedSet:self.images];
+    [tempSet addObject:value];
+    self.images = tempSet;
+}
+
+- (void)removeImages:(NSOrderedSet<Image *> *)values {
+    NSMutableOrderedSet* tempSet = [NSMutableOrderedSet orderedSetWithOrderedSet:self.images];
+    [tempSet removeObjectsInArray:values.array];
+    self.images = tempSet;
 }
 
 @end
